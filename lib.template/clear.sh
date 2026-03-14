cd747f1419c67eb245e924073b1296629b5f6148
Prool <proolix@gmail.com>
2026-03-14 16:15:40 +0100

    add zone utilities scripts

---
+++ lib.template/clear.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# delete all zone files
+rm zon/*.zon
+rm trg/*.trg
+rm wld/*.wld
+rm mob/*.mob
+rm obj/*.obj
---
+++ lib.template/zcopy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# copy zone files from another world
+cp ../world.vmud/obj/$1* obj
+cp ../world.vmud/mob/$1* mob
+cp ../world.vmud/trg/$1* trg
+cp ../world.vmud/wld/$1* wld
+cp ../world.vmud/zon/$1* zon
