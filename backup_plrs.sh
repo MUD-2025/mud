b84105aad5c9c112ca811b8d521c73d5d8c95cd6
Prool <proolix@gmail.com>
2026-06-02 21:34:39 +0200

    cosmetics

--- backup_plrs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
-PLRS_BACKUP_PREFIX=plrs
-
-tar -cpjf ${DIRECTORY}/backup/${PLRS_BACKUP_PREFIX}.$(date +%d%m%y-%H%M%S).tgz ${DIRECTORY}/lib/{plrs,plrobjs,plrstuff,plrvars,etc,plralias}
-find ${DIRECTORY}/backup/ -name "${PLRS_BACKUP_PREFIX}.*" -atime 2 -exec rm '{}' \;
-
-# vim: set ts=4 sw=4 tw=0 noet :
